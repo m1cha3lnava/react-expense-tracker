4ae2650ec5a7b2a9dde6676bb93c3b2a1c9b9a15
Michael Nava <M1cha3l.nava@gmail.com>
2020-08-12 20:51:20 -0400

    added components and context files

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
-import Header from '../../components/Header';
+import React, { Component } from "react";
+import Header from "../../components/Header";
+import "../../App.css";
+import Balance from "../../components/Balance";
+import IncomeExp from "../../components/IncomeExp";
+import TransactionList from "../../components/TransactionList";
+import AddTransaction from "../../components/AddTransaction";
+import { GlobalProvider } from "../../context//GlobalState";
+
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                <Header />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <GlobalProvider>
+        <Header />
+        <div className="container">
+          <Balance />
+          <IncomeExp />
+          <TransactionList />
+          <AddTransaction />
+        </div>
+      </GlobalProvider>
+    );
+  }
 }
 
 export default Home;
