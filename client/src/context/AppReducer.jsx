ab6538414d703f1860d19070335bccec004d9bfb
Michael Nava <M1cha3l.nava@gmail.com>
2020-08-24 12:19:16 -0400

    the app reducer changed to if statements

--- client/src/context/AppReducer.jsx
+++ client/src/context/AppReducer.jsx
@@ -1,18 +1,17 @@
 export default (state, action) => {
-  switch (action.type) {
-    case "DELETE_TRANSACTION":
-      return {
-        ...state,
-        transactions: state.transactions.filter(
-          (transaction) => transaction.id !== action.payload
-        ),
-      };
-    case "ADD_TRANSACTION":
-      return {
-        ...state,
-        transactions: [action.payload, ...state.transactions],
-      };
-    default:
-      return state;
+  if (action.type === "DELETE_TRANSACTION") {
+    return {
+      ...state,
+      transactions: state.transactions.filter(
+        (transaction) => transaction.id !== action.payload
+      ),
+    };
+  } else if (action.type === "ADD_TRANSACTION") {
+    return {
+      ...state,
+      transactions: [action.payload, ...state.transactions],
+    };
+  } else {
+    return "error with transaction";
   }
 };
